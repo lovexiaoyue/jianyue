d254acda216b4df292fdd92fe37cb939bc9b0f36
lovexiaoyue <1029644672@qq.com>
2018-11-26 14:22:52 +0800

    短信验证码完成

---
+++ jianyue/jianyue/apps/verifications/serializers.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+from django_redis import get_redis_connection
+import logging
+logger = logging.getLogger('loggers')
+
+class ImageCodeCheckSerializer(serializers.Serializer):
+    """
+    图片验证码校验序列化器
+    """
+    image_code_id = serializers.UUIDField()
+    text = serializers.CharField(max_length=4, min_length=4)
+
+    def validate(self, attrs):
+        """
+        校验
+        """
+        image_code_id = attrs['image_code_id']
+        text = attrs['text']
+
+        # 查询验证码
+        redis_conn = get_redis_connection("verify_codes")
+        r_code = redis_conn.get("img_%s" %image_code_id)
+        if not r_code:
+            raise serializers.ValidationError('图片验证码无效')
+        try:
+            redis_conn.delete("img_%s" % image_code_id)
+        except Exception as e:
+            logger.error(e)
+        # 进行比对
+        r_code = r_code.decode()
+        if r_code.lower() != text.lower():
+            raise serializers.ValidationError('图片验证码错误')
+        # 检测是否在60S内
+        mobile = self.context['view'].kwargs['mobile']
+        send_flag = redis_conn.get("send_flag_%s" %mobile)
+        if send_flag:
+            raise serializers.ValidationError('请求过于频繁')
+
+        return attrs
+
---
+++ jianyue/jianyue/apps/verifications/views.py
@@ -0,0 +1,69 @@
+from rest_framework.views import APIView
+from rest_framework.generics import GenericAPIView
+from django_redis import get_redis_connection
+from jianyue.utils.captcha.captcha import captcha
+from django.http import HttpResponse
+from jianyue.libs import constants
+from rest_framework import status
+from rest_framework.response import Response
+from jianyue.libs.yuntongxun.sms import CCP
+import random
+from . import serializers
+import logging
+logger = logging.getLogger('loggers')
+
+class ImageCodeView(APIView):
+    """
+    图片验证码
+    """
+
+    def get(self, request, image_code_id):
+        """
+        获取图片验证码
+        """
+
+        # 生成验证码图片
+        text, image = captcha.generate_captcha()
+        print(text)
+        code_id = image_code_id
+        print(code_id)
+        redis_conn = get_redis_connection("verify_codes")
+        redis_conn.setex("img_%s" % code_id, constants.IMAGE_CODE_REDIS_EXPIRES, text)
+
+        return HttpResponse(image, content_type="images/jpg")
+
+
+class SMSCodeView(GenericAPIView):
+    """
+    短信验证码
+    """
+    serializer_class = serializers.ImageCodeCheckSerializer
+
+    def get(self, request, mobile):
+
+        serializer = self.get_serializer(data=request.query_params)
+        serializer.is_valid(raise_exception=True)
+
+        # 生成短信验证码
+        sms_code = "%04d" % random.randint(0,999999)
+
+        # 保存验证码记录
+        redis_conn = get_redis_connection("verify_codes")
+        pl = redis_conn.pipeline()
+        pl.multi()
+        pl.setex("sms_%s" %mobile, constants.SMS_CODE_REDIS_EXPIRES, sms_code)
+        pl.setex("send_flag_%s" %mobile, constants.SMS_SEND_FLAG_REDIS_EXPIRES, 1)
+        pl.execute()
+        # 发送短信
+        try:
+            ccp = CCP()
+            result = ccp.send_template_sms(mobile, [sms_code, 5], 1)
+            if result == 0:
+                return Response({"message":"success"},status.HTTP_200_OK)
+            if result == -1:
+                return Response({"message":"fall"}, status.HTTP_200_OK)
+            else:
+                return Response({"message":"error"}, status.HTTP_501_NOT_IMPLEMENTED)
+        except Exception as e:
+            logger.error(e)
+            return Response({"message": "error"}, status.HTTP_501_NOT_IMPLEMENTED)
